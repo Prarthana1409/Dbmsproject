1f44b2c4044d6eb48e99de0d2f7e25d2cb645f1a
Prarthana1409 <prarthana.cclub@gmail.com>
2022-01-17 12:59:43 +0530

    push

---
+++ clinicaldb/src/clinicaldb/Clinicaldb.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package clinicaldb;
+
+/**
+ *
+ * @author Prarthana
+ */
+public class Clinicaldb {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
